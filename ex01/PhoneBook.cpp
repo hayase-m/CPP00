405ec1619acd095fdd2b64304b8f48ae0c578524
Hayase Maruyama <mrym.124.hys@gmail.com>
2026-03-06 18:08:51 +0900

    feat:displayAllContactsの実装

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -1,10 +1,27 @@
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook() : _contactIndex(0), _sumContacts(0) {}
+PhoneBook::PhoneBook() : _contactIndex(0), _contactsCount(0) {}
 
-void PhoneBook::AddContact(const Contact &contact) {
+void PhoneBook::addContact(const Contact &contact) {
   _contacts[_contactIndex] = contact;
   _contactIndex = (_contactIndex + 1) % 8;
-  if (_sumContacts < 8)
-    _sumContacts++;
+  if (_contactsCount < 8)
+    _contactsCount++;
 }
+
+std::string PhoneBook::_formatField(const std::string &field) {
+  if (field.length() > 10)
+    return field.substr(0, 9) + '.';
+  return field;
+};
+
+void PhoneBook::displayAllContacts() {
+  for (size_t i = 0; i < _contactsCount; i++) {
+    std::cout << '|' << std::setw(10) << i << '|' << std::setw(10)
+              << _formatField(_contacts[i].GetFirstName()) << '|'
+              << std::setw(10) << _formatField(_contacts[i].GetLastName())
+              << '|' << std::setw(10)
+              << _formatField(_contacts[i].GetNickName()) << '|' << std::endl;
+  }
+}
+void PhoneBook::displayContact(size_t searchIndex) {};
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -3,17 +3,21 @@
 
 #include "Contact.hpp"
 #include <cstddef>
+#include <iomanip>
+#include <iostream>
 
 class PhoneBook {
 private:
   Contact _contacts[8];
   size_t _contactIndex;
-  size_t _sumContacts;
+  size_t _contactsCount;
+  std::string _formatField(const std::string &field);
 
 public:
   PhoneBook();
-  void AddContact(const Contact &contact);
-  Contact ShowContact(size_t searchIndex);
+  void addContact(const Contact &contact);
+  void displayAllContacts();
+  void displayContact(size_t searchIndex);
 };
 
 #endif // PHONEBOOK_HPP
