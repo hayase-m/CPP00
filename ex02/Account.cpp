277090dacfa104ea60b87d196bfd80f8d917e59c
Hayase Maruyama <mrym.124.hys@gmail.com>
2026-04-21 11:33:25 +0900

    fix:コンパイルエラーの修正

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,4 +1,4 @@
-#include <Account.hpp>
+#include "Account.hpp"
 #include <iomanip>
 #include <iostream>
 
@@ -33,21 +33,21 @@ Account::~Account(void) {
   _displayTimestamp();
   std::cout << " index:" << _accountIndex << ";amount:" << _amount << ";closed"
             << std::endl;
-};
+}
 
 void Account::displayAccountsInfos(void) {
   _displayTimestamp();
   std::cout << " accounts:" << _nbAccounts << ":total:" << _totalAmount
             << ";deposits:" << _totalNbDeposits
             << ";withdrawals:" << _totalNbWithdrawals << std::endl;
-};
+}
 
 void Account::displayStatus(void) const {
   _displayTimestamp();
   std::cout << " index:" << _accountIndex << ";amount:" << _amount
             << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals
             << std::endl;
-};
+}
 
 void Account::makeDeposit(int deposit) {
   int p_amount = _amount;
@@ -60,7 +60,8 @@ void Account::makeDeposit(int deposit) {
   std::cout << " index:" << _accountIndex << ";p_amount:" << p_amount
             << ";deposit:" << deposit << ";amount" << _amount
             << ";nb_deposits:" << _nbDeposits << std::endl;
-};
+}
+
 bool Account::makeWithdrawal(int withdrawal) {
   int p_amount = _amount;
   if (withdrawal <= p_amount) {
@@ -73,15 +74,17 @@ bool Account::makeWithdrawal(int withdrawal) {
     std::cout << " index:" << _accountIndex << ";p_amount:" << p_amount
               << ";withdrawal:" << withdrawal << ";amount" << _amount
               << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+    return true;
   } else {
     _displayTimestamp();
     std::cout << " index:" << _accountIndex << ";p_amount:" << p_amount
               << ";withdrawal:refused" << std::endl;
+    return false;
   }
-};
+}
 
 int Account::getNbAccounts(void) { return _nbAccounts; }
 int Account::getTotalAmount(void) { return _totalAmount; }
 int Account::getNbDeposits(void) { return _totalNbDeposits; }
 int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
-int Account::checkAmount(void) const { return _amount; };
+int Account::checkAmount(void) const { return _amount; }
